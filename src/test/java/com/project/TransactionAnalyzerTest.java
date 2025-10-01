0f6bfda055c18567902dae031536bdf0e7217a73
Vadym <vadim.rolex.2005@gmail.com>
2025-10-01 18:57:02 +0300

    Developed unit test for calculating total cost.

---
+++ src/test/java/com/project/TransactionAnalyzerTest.java
@@ -0,0 +1,23 @@
+package com.project;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
+
+class TransactionAnalyzerTest {
+
+    @Test
+    public void testCalculateTotalBalance() {
+        Transaction transaction1 = new Transaction("2023-01-01", 100.0, "Дохід");
+        Transaction transaction2 = new Transaction("2023-01-02", -50.0, "Витрата");
+        Transaction transaction3 = new Transaction("2023-01-03", 150.0, "Дохід");
+
+        List<Transaction> transactions = Arrays.asList(transaction1, transaction2, transaction3);
+        TransactionAnalyzer analyzer = new TransactionAnalyzer(transactions);
+
+        double result = analyzer.calculateTotalBalance();
+
+        Assertions.assertEquals(200.0, result, "Розрахунок загального балансу неправильний");
+    }
+}
